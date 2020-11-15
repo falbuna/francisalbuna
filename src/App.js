aafac23e291f133b7457777d1127039fb9b263b5
Francis Albuna <falbuna1@gmail.com>
2020-11-15 12:33:57 -0800

    Added the initial files and folders.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+    <div>
+        <Route exact path="/" component={About} /> 
+        <Route exact path="/about" component={About} />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
