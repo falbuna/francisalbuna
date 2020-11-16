62ea975647e584d24be6cb674a5218bdadf076be
Francis Albuna <falbuna1@gmail.com>
2020-11-15 18:08:51 -0800

    Got Tailwind installed for React. Got a Navbar template setup.

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function About(){
     return(
-        <div>
-            <p>Test</p>
+        <div className="h-64">
+            <div className="p-4 m-4 bg-skyBlue">
+                <h1 className="text-2xl font-bold text-white">Tailwind CSS Practice</h1>
+            </div>
+            <div className="p-4 m-4 bg-blueLight1 h-full">
+                <h2 className="text-black">Having fun practicing Tailwind CSS with React</h2>
+                <button className="bg-baby1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My Tailwind Button</button>
+            </div>
         </div>
     )
 }
