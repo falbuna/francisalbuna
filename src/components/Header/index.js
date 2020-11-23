c7d3015cb41d153494617c3d13a33fb29b8d5908
Francis Albuna <falbuna1@gmail.com>
2020-11-22 20:40:55 -0800

    Got the Navbar to be mobile responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import resume from "../../assets/documents/Francis_Albuna_Resume.pdf"
 
-function Header() {
-    
+function Header( { fixed}) {
+
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
 return (
 
 <nav className="bg-blueLight1">
@@ -10,7 +12,7 @@ return (
     <div className="relative flex items-center justify-between h-16">
     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
         
-        <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
+        <button className="inline-flex items-center justify-center p-2 rounded-md bg-gray-200 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false" onClick={() => setNavbarOpen(!navbarOpen)}>
           
           <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -38,12 +40,14 @@ return (
     </div>
   </div>
 
-  <div className="hidden sm:hidden">
-    <div className="px-2 pt-2 pb-3">
-      <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
-      <a href="About" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">About</a>
-      <a href="Portfolio" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Portflio</a>
-      <a href="Contact" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Contact</a>
+  <div className={
+    (navbarOpen ? "sm:hidden" : "hidden")
+  }>
+    <div className="px-2 pt-2 pb-3 border-b border-gray-400">
+      <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Resume</a>
+      <a href="About" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">About</a>
+      <a href="Portfolio" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Portflio</a>
+      <a href="Contact" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Contact</a>
     </div>
   </div>
 </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import bio from "../assets/images/bio-image.jpg"
+import bio from "../assets/images/bio-image.jpg";
 
 function About(){
     return(
@@ -26,13 +26,6 @@ function About(){
             <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
               <img className="rounded-lg shadow-lg object-cover object-center" src={bio} alt="Francis on a beach in Boracay." width="1184" height="1376" />
             </div>
-            {/* <figcaption className="mt-3 flex text-sm text-gray-500"> */}
-              
-              {/* <svg className="flex-none w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-              </svg> */}
-              {/* <span className="ml-2">Photograph by Marcus O'Leary</span> */}
-            {/* </figcaption> */}
           </figure>
         </div>
       </div>
